08fa22e9b2b4ec8f3a670f160e2a5b437e9108c4
juno-junho <ssmm0205@naver.com>
2024-06-27 16:26:32 +0900

    feat: project setup

---
+++ src/main/java/com/junho/flow/FlowApplication.java
@@ -0,0 +1,13 @@
+package com.junho.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowApplication.class, args);
+    }
+
+}
