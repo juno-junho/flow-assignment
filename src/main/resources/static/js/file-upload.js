086b83147aacd4d2e50b7d05fa9441602a868495
juno-junho <ssmm0205@naver.com>
2024-07-05 17:53:23 +0900

    refactor(js) file-upload.js 리팩토링

--- src/main/resources/static/js/file-upload.js
+++ src/main/resources/static/js/file-upload.js
@@ -1,37 +1,54 @@
-document.getElementById('fileUploadForm').addEventListener('submit', function (e) {
-    e.preventDefault();
+document.getElementById('fileUploadForm').addEventListener('submit', async function (e) {
+    e.preventDefault(); // 기본 동작 방지 (폼 제출 후 새고로침)
 
     const fileInput = document.getElementById('formFileMultiple');
-    if (fileInput.files.length === 0) {
-        alert('파일을 선택해 주세요.');
+    if (isEmptyFile(fileInput)) {
         return;
     }
 
-    const formData = new FormData(this);
-    fetch(this.action, {
-        method: 'POST',
-        body: formData, // FormData를 POST 요청 본문으로 설정
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('서버에서 오류가 발생했습니다. 상태 코드: ' + response.status);
-            }
-            return response.text(); // 응답을 텍스트로 변환
-        })
-        .then(text => {
-            if (text) {
-                const data = JSON.parse(text); // 응답 텍스트를 JSON으로 파싱
-                if (data.message) {
-                    alert(data.message); // JSON에 message 필드가 있으면 이를 표시
-                } else {
-                    alert('파일 업로드 성공!'); // JSON 응답이지만 message 필드가 없는 경우
-                }
-            } else {
-                alert('파일 업로드 성공!'); // 응답 본문이 비어있는 경우
-            }
-        })
-        .catch(error => {
-            console.error('Error during upload:', error);
-            alert('파일 업로드 중 오류 발생: ' + error.message); // 오류 메시지 표시
-        });
+    try {
+        const response = await uploadFiles(this);
+        handleUploadResponse(response);
+    }catch (error){
+        console.error('Error during upload:', error);
+        alert('파일 업로드 중 오류 발생: ' + error.message); // 오류 메시지 표시
+    }
 });
+
+const isEmptyFile = (input) => {
+    if (input.files.length === 0) {
+        alert('파일을 선택해 주세요.');
+        return true;
+    }
+    return false;
+}
+
+const uploadFiles = async (form) => {
+    const formData = new FormData(form);
+    const response = await fetch(form.action, {
+        method: 'POST',
+        body: formData
+    });
+
+    if (!response.ok) {
+        throw new Error('서버에서 오류가 발생했습니다. 상태 코드: ' + response.status);
+    }
+    return response.text();
+}
+
+const handleUploadResponse = (text) => {
+    if (text) {
+        const data = JSON.parse(text); // 응답 텍스트를 JSON으로 파싱
+        validateData(data);
+        return;
+    }
+    alert('파일 업로드 성공!'); // 응답 본문이 비어있는 경우
+}
+
+const validateData = (data) => {
+    if (data.message) { // JSON에 message 필드가 있으면 이를 표시
+        alert(data.message);
+    } else {
+        alert('파일 업로드 성공!'); // JSON 응답이지만 message 필드가 없는 경우
+    }
+}
